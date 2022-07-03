2f9c14edf82b7fe0412c99d2242dbe403a3b6697
wadebooth <wademgmt@gmail.com>
2022-07-03 18:20:38 -0400

    completed maxheight

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -9,13 +9,14 @@ export default function Flashcard({ flashcard }) {
   function setMaxHeight() {
     const frontHeight = frontEl.current.getBoundingClientRect().height
     const backHeight = backEl.current.getBoundingClientRect().height
-    Math.max(frontHeight, backHeight, 100)
+    setHeight(Math.max(frontHeight, backHeight, 100))
   }
 
-  useEffect(() => {}, [flashcard.question, flashcard.answer])
+  useEffect(setMaxHeight, [flashcard.question, flashcard.answer])
   return (
     <div
       className={`card ${flip ? 'flip' : ''}`}
+      style={{ height: height }}
       onClick={() => setFlip(!flip)}
     >
       <div className='front' ref={frontEl}>
