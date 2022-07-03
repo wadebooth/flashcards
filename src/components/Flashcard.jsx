f049b94e06f70ad55b8e3e5da9db12653cf25419
wadebooth <wademgmt@gmail.com>
2022-07-03 18:17:42 -0400

    added useRef and useEffect for setting the max height of the card

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false)
+  const [height, setHeight] = useState('initial')
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    Math.max(frontHeight, backHeight, 100)
+  }
+
+  useEffect(() => {}, [flashcard.question, flashcard.answer])
   return (
     <div
       className={`card ${flip ? 'flip' : ''}`}
       onClick={() => setFlip(!flip)}
     >
-      <div className='front'>
+      <div className='front' ref={frontEl}>
         {flashcard.questions}
         {/* <div className='flashcard-options'>
           {flashcard.option.map((option) => {
@@ -15,7 +26,9 @@ export default function Flashcard({ flashcard }) {
           })}
         </div> */}
       </div>
-      <div className='back'>{flashcard.answer}</div>
+      <div className='back' ref={backEl}>
+        {flashcard.answer}
+      </div>
     </div>
   )
 }
