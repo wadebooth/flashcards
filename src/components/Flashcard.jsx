02340b6ce74f227cb7f04663cfe031ee1c2a41b2
wadebooth <wademgmt@gmail.com>
2022-07-03 17:01:12 -0400

    added flip ability

--- src/components/Flashcard.jsx
+++ src/components/Flashcard.jsx
@@ -3,7 +3,10 @@ import React, { useState } from 'react'
 export default function Flashcard({ flashcard }) {
   const [flip, setFlip] = useState(false)
   return (
-    <div onClick={() => setFlip(!flip)}>
+    <div
+      clsasName={`card ${flip ? 'flip' : ''}`}
+      onClick={() => setFlip(!flip)}
+    >
       {flip ? flashcard.answer : flashcard.question}
     </div>
   )
