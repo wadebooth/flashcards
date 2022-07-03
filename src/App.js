d43a8b3acd63955b9a0b1cada4e104683eb90541
wadebooth <wademgmt@gmail.com>
2022-07-03 16:47:05 -0400

    removed hints from flashcard object

--- src/App.js
+++ src/App.js
@@ -13,88 +13,74 @@ const SAMPLE_FLASHCARDS = [
     id: 1,
     question: 'What are hashmaps/Map Objects in Javascript?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 2,
     question: 'What tools do you use for version control?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 3,
     question: 'What is a REST API?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 4,
     question: 'In a relational database, how can you connect 2 objects/tables?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 5,
     question: 'What is a terminal server?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 6,
     question: 'How do you communicate with a database in React?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 7,
     question: 'How do you make sure your app is up and running 24/7?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 8,
     question: 'What is DOM and Virtual DOM in React?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 9,
     question: 'What is an interface in Typescript?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 10,
     question:
       'What is the difference between arrow functions and regular functions?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 11,
     question: 'What is JSX?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 12,
     question:
       'What is the difference between named exports and default exports?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 13,
     question:
       'What is the difference between functional components and class based components?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
   {
     id: 14,
     question: 'What is a pure component?',
     answer: 'Answer',
-    options: ['2', '3', '4', '5'],
   },
 ]
 
